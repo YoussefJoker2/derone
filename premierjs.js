e26fe9db37765a41658d48a2bd9cd8656b27f40e
YoussefJoker2 <333king.yousef@gmail.com>
2025-07-19 19:41:13 +0300

    Add files via upload

---
+++ premierjs.js
@@ -0,0 +1,49 @@
+var imgderone = document.getElementsByClassName("imagederone");
+
+
+imgderone[0].addEventListener("click", function(){
+
+   imgderone[0].src="https://i.postimg.cc/BZNjX0jZ/2025-07-19-16-32-45-b60abfe6.jpg ";
+});
+
+imgderone[1].addEventListener("click", function(){
+
+   imgderone[1].src="https://i.postimg.cc/PrySFBpC/2025-07-19-152857.png ";
+});
+
+imgderone[2].addEventListener("click", function(){
+ 
+   imgderone[2].src="https://i.postimg.cc/7YcjV55B/2025-07-19-162303.png  ";
+});
+
+imgderone[3].addEventListener("click", function(){
+ 
+   imgderone[3].src="https://i.postimg.cc/VvD60j4L/2025-07-19-17-06-18-5b9bf5c8.jpg ";
+});
+
+imgderone[4].addEventListener("click", function(){
+ 
+   imgderone[4].src="https://i.postimg.cc/7Y8JFVJ0/2025-07-19-16-50-58-638ac85c.jpg  ";
+});
+
+imgderone[5].addEventListener("click", function(){
+ 
+   imgderone[5].src="https://i.postimg.cc/xdzcpNjt/2025-07-19-170829.png   ";
+});
+
+imgderone[6].addEventListener("click", function(){
+ 
+   imgderone[6].src="https://i.postimg.cc/dV71DZCw/2025-07-19-17-10-58-ca3727fa.jpg   ";
+});
+
+imgderone[7].addEventListener("click", function(){
+ 
+   imgderone[7].src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqj1d1UERWdXUkFc_9N3iMueGXv_tjoY-yDA&s  ";
+});
+
+imgderone[8].addEventListener("click", function(){
+ 
+   imgderone[8].src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSqj1d1UERWdXUkFc_9N3iMueGXv_tjoY-yDA&s  ";
+   imgderone[8].style.width="70%"; 
+   imgderone[8].style.height="30%";
+});
